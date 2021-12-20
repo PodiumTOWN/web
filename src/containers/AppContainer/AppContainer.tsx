f9f6ea21ff52ba55a2f25626793aaab6db36e0ab
Michael Jach <michaljach@gmail.com>
2021-12-21 00:41:39 +0100

    privacy policy

--- src/containers/AppContainer/AppContainer.tsx
+++ src/containers/AppContainer/AppContainer.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { Route, Routes } from 'react-router-dom';
 import PostContainer from '../PostContainer/PostContainer';
+import PrivacyContainer from '../PrivacyContainer/PrivacyContainer';
 
 function AppContainer() {
   return (
     <Routes>
       <Route path="/:username/:postId" element={<PostContainer />} />
+      <Route path="/privacy" element={<PrivacyContainer />} />
     </Routes>
   );
 }
