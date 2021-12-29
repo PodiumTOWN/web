af190d52d1a64a28d408a71aed7cbdb4b52d4d89
Michael Jach <michaljach@gmail.com>
2021-12-29 13:18:18 +0100

    eual

--- src/containers/AppContainer/AppContainer.tsx
+++ src/containers/AppContainer/AppContainer.tsx
@@ -3,12 +3,14 @@ import 'antd/dist/antd.css';
 import { Route, Routes } from 'react-router-dom';
 import PostContainer from '../PostContainer/PostContainer';
 import PrivacyContainer from '../PrivacyContainer/PrivacyContainer';
+import TermsContainer from '../TermsContainer/TermsContainer';
 
 function AppContainer() {
   return (
     <Routes>
       <Route path="/:username/:postId" element={<PostContainer />} />
       <Route path="/privacy" element={<PrivacyContainer />} />
+      <Route path="/eula" element={<TermsContainer />} />
     </Routes>
   );
 }
